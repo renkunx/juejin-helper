3f8f6fae4eef4081b8df4271c8c33f70c48a00e1
renkunx <rk-tingyu@outlook.com>
2023-03-16 05:47:28 +0000

    feat: 增加蜗牛阅读自动签到

--- workflows/utils/env.js
+++ workflows/utils/env.js
@@ -29,5 +29,8 @@ module.exports = {
    * 企业微信机器人配置
    * https://developer.work.weixin.qq.com/document/path/91770
    */
-  WEIXIN_WEBHOOK: env.WEIXIN_WEBHOOK
+  WEIXIN_WEBHOOK: env.WEIXIN_WEBHOOK,
+  // 蜗牛阅读 token & cookie
+  DU_TOKEN: env.DU_TOKEN,
+  DU_COOKIE: env.DU_COOKIE,
 };
